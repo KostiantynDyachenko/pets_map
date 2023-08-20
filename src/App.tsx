22987e4b79e549e3c64f782c70af342a0acbf7cd
Kostya Dyachenko <132937369+KostiantynDyachenko@users.noreply.github.com>
2023-08-20 13:51:43 +0300

    added useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,53 @@
 import React from 'react';
-import { Map, Pet } from './components/Map';
+import {Map, Pet} from './components/Map';
 import {
-  QueryClient,
-  QueryClientProvider,
+    QueryClient,
+    QueryClientProvider, useQuery,
 } from 'react-query';
 
 const queryClient = new QueryClient();
 
 const petsData: Array<Pet> = [
-  {
-    id: '1',
-    name: 'Buddy',
-    breed: {
-      name: 'Dog',
+    {
+        id: '1',
+        name: 'Buddy',
+        breed: {
+            name: 'Dog',
+        },
+        price: 100,
+        pricingType: 'money',
+        location: [52.5200, 13.4050],
+        images: [{
+            id: '1',
+            urls: {
+                card: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+            },
+        }]
     },
-    price: 100,
-    pricingType: 'money',
-    location: [ 52.5200, 13.4050],
-    images: [{
-      id: '1',
-      urls: {
-        card: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
-      },
-    }]
-  },
- 
+
 ];
 
 function App() {
 
-  const queryFunc = (query:{ location: string,radius: number}) =>{
-    return fetch('').then((response) => response.json())
-  }
+    function usePetQuery({location, radius}: { location: { lat: number, lng: number }, radius: number }) {
+        const queryKey = ['pets', location, radius];
+        const fetchPets = async () => {
+            // Fetch pets data using location and radius
+            return fetch('https://jsonplaceholder.typicode.com/todos/1')
+        };
+
+        return useQuery(queryKey, fetchPets);
+    }
 
 
-  return (
-    <QueryClientProvider client={queryClient}>
+    return (
+        <QueryClientProvider client={queryClient}>
 
-     <Map pets={petsData} queryFunc={queryFunc} />
-    </QueryClientProvider>
+            <Map pets={petsData} useQuery={usePetQuery}/>
 
-  );
+        </QueryClientProvider>
+
+    );
 }
 
 export default App
